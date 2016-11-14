806a3157b4c26449368501a88fe4460b7fa442de
chunzhiying <chun.zhiying.ggl@gmail.com>
2016-11-14 10:55:03 +0800

    添加 block回调

--- HWWeakTimer/HWWeakTimer.h
+++ HWWeakTimer/HWWeakTimer.h
@@ -8,11 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
+typedef void(^TimerBlock)(NSDictionary *userInfo);
+
 @interface HWWeakTimer : NSObject
 
 // NSString * const NSDefaultRunLoopMode;
 // NSString * const NSRunLoopCommonModes NS_AVAILABLE(10_5, 2_0);
 
+#pragma mark - Selector
 + (nonnull NSTimer *) scheduledTimerWithTimeInterval:(NSTimeInterval)interval
                                       target:(nonnull id)aTarget
                                     selector:(nonnull SEL)aSelector
@@ -26,5 +29,21 @@
                            repeats:(BOOL)yesOrNo
                            runloop:(nonnull NSRunLoop *)runloop
                               mode:(nonnull NSString *)mode;
+
+
+#pragma mark - Block
++ (nonnull NSTimer *) scheduledTimerWithTimeInterval:(NSTimeInterval)interval
+                                              target:(nonnull id)aTarget
+                                            userInfo:(nullable id)userInfo
+                                             repeats:(BOOL)repeats
+                                            callBack:(nonnull TimerBlock)block;
+
++ (nonnull NSTimer *)timerWithTimeInterval:(NSTimeInterval)ti
+                                    target:(nonnull id)aTarget
+                                  userInfo:(nullable id)userInfo
+                                   repeats:(BOOL)yesOrNo
+                                   runloop:(nonnull NSRunLoop *)runloop
+                                      mode:(nonnull NSString *)mode
+                                  callBack:(nonnull TimerBlock)block;
 
 @end
