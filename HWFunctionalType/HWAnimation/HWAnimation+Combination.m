a001aba0ae28f2ccd95c9ed57f341b063631016b
chunzhiying <chun.zhiying.ggl@gmail.com>
2016-12-19 12:01:21 +0800

    组合动画优化

--- HWFunctionalType/HWAnimation/HWAnimation+Combination.m
+++ HWFunctionalType/HWAnimation/HWAnimation+Combination.m
@@ -14,19 +14,24 @@
     return ^(CGRect rect) {
         CGPoint toPositon = CGPointMake(rect.origin.x + rect.size.width / 2, rect.origin.y + rect.size.height / 2);
         CGRect toBounds = CGRectMake(0, 0, rect.size.width, rect.size.height);
-        return  @[HWAnimInstance.animate(HW_Animation_Basic, @"position").to([NSValue valueWithCGPoint:toPositon]),
-                  HWAnimInstance.animate(HW_Animation_Basic, @"bounds").to([NSValue valueWithCGRect:toBounds])]
-        .animationGroup.fillMode(HW_FillMode_Retain);
+        return self
+        .animateGroup()
+        .animations(@[HWAnimInstance.animate(HW_Animation_Basic, @"position").to([NSValue valueWithCGPoint:toPositon]),
+                      HWAnimInstance.animate(HW_Animation_Basic, @"bounds").to([NSValue valueWithCGRect:toBounds])])
+        .fillMode(HW_FillMode_Retain);
     };
 }
 
 - (HWAnimation *(^)(CGFloat, CGFloat, CGFloat, CGFloat))scaleBounce {
     return ^(CGFloat start, CGFloat middle, CGFloat end, CGFloat duration) {
-        return  @[HWAnimInstance.animate(HW_Animation_Basic, @"transform.scale")
-                  .from(@(start)).to(@(middle)).duration(duration),
-                  HWAnimInstance.animate(HW_Animation_Basic, @"transform.scale")
-                  .from(@(middle)).to(@(end)).duration(duration).beginTime(duration)]
-        .animationGroup.fillMode(HW_FillMode_Retain).duration(2 * duration);
+        return self
+        .animateGroup()
+        .animations(@[HWAnimInstance.animate(HW_Animation_Basic, @"transform.scale")
+                        .from(@(start)).to(@(middle)).duration(duration),
+                      HWAnimInstance.animate(HW_Animation_Basic, @"transform.scale")
+                        .from(@(middle)).to(@(end)).duration(duration).beginTime(duration)])
+        .fillMode(HW_FillMode_Retain)
+        .duration(2 * duration);
     };
 }
 
